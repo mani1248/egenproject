994b3bb451f0b5140be1e0950f49552f7fd208dd
mani1248 <manish_tammali@hotmail.com>
2015-10-02 20:30:53 -0400

    bootstrap and carousel

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,3 +1,13 @@
 /**
  * Created by Manish on 9/23/2015.
  */
+
+
+//$(function(){
+//    if (window.jQuery) {
+//        console.log("Loaded");
+//    } else
+//    console.log("not loaded");
+//
+//});
+
